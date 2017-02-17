4a155322c318af514419d1e2d4355caae679b134
Maia Stone <maiadov@gmail.com>
2017-02-16 21:04:58 -0700

    Set up server and react

---
+++ lib/components/Application.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Application extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello Other World</h2>
+      </div>
+    );
+  }
+}
+module.exports = Application;
