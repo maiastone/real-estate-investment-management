771c171456dcd449e57d9c5d5c92d619c6a87634
Maia Stone <maiadov@gmail.com>
2017-02-17 20:18:52 -0700

    WIP config for heroku

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const app = express();
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
-const app = express();
-const http = require('http');
 const path = require('path');
+const http = require('http');
 
 const compiler = webpack(webpackConfig);
 
-const environment = process.env.PORT || 8080;
+const environment = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || 8080;
+
+const server = http.createServer(app)
+  .listen(port, () => {
+    console.log(`Listening on port ${port}.`);
+  });
+
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
+
 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
@@ -21,9 +29,3 @@ app.use(webpackDevMiddleware(compiler, {
   },
   historyApiFallback: true,
 }));
-
-const server = app.listen(environment, function() {
-  const host = server.address().address;
-  const port = server.address().port;
-  console.log('Listening at http://%s:%s', host, port);
-});
