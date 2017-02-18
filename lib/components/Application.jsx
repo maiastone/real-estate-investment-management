f6685eae3a3a75dd88bb3ca15d3659a592886957
Maia Stone <maiadov@gmail.com>
2017-02-18 11:24:30 -0700

    Verify .env is in gitignore

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes as T } from 'react';
 
 export default class Application extends Component {
   constructor() {
@@ -9,9 +9,16 @@ export default class Application extends Component {
   }
 
   render() {
+    let children = null;
+    if (this.props.children) {
+      children = React.cloneElement(this.props.children, {
+        auth: this.props.route.auth
+      })
+    }
     return (
       <div>
         <h2>Hello Other World</h2>
+        {children}
       </div>
     );
   }
