8b4ce19b836f05457409278537022e2030bc5c6d
Maia Stone <maiadov@gmail.com>
2017-02-19 14:34:08 -0700

    Add toggle income debt button

--- lib/components/PropertyContainer.jsx
+++ lib/components/PropertyContainer.jsx
@@ -6,6 +6,32 @@ export default class PropertyContainer extends React.Component {
   render() {
     return (
       <div>
+        <div className="switch">
+          <input type="radio"
+            className="switch-input"
+            name="view"
+            value="income"
+            id="income"
+            defaultChecked>
+          </input>
+          <label htmlFor="income"
+            className="switch-label switch-label-off"
+            >Income
+          </label>
+
+          <input type="radio"
+            className="switch-input"
+            name="view"
+            value="debt"
+            id="debt">
+          </input>
+          <label htmlFor="debt"
+            className="switch-label switch-label-on"
+            >Debt
+          </label>
+
+          <span className="switch-selection"></span>
+        </div>
         <PropertyCard />
       </div>
     );
